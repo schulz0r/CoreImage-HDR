39c483107b1b64903ce95cd1d956cb4b3c0250b7
Philipp Waxweiler <philipp@waxweiler.de>
2017-11-09 15:22:15 +0100

    Initial Commit

---
+++ CoreImage-HDR/CoreImage_HDR.h
@@ -0,0 +1,19 @@
+//
+//  CoreImage_HDR.h
+//  CoreImage-HDR
+//
+//  Created by Philipp Waxweiler on 09.11.17.
+//  Copyright © 2017 Philipp Waxweiler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreImage_HDR.
+FOUNDATION_EXPORT double CoreImage_HDRVersionNumber;
+
+//! Project version string for CoreImage_HDR.
+FOUNDATION_EXPORT const unsigned char CoreImage_HDRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreImage_HDR/PublicHeader.h>
+
+
