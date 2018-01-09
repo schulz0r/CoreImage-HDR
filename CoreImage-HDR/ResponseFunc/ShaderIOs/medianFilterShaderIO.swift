75739dd8522d706dad0753f1931ecf0aecc18870
Philipp Waxweiler <philipp@waxweiler.de>
2018-01-09 20:19:32 +0100

    added median filter shader io

---
+++ CoreImage-HDR/ResponseFunc/ShaderIOs/medianFilterShaderIO.swift
@@ -0,0 +1,9 @@
+//
+//  medianFilterShaderIO.swift
+//  CoreImage-HDR
+//
+//  Created by Philipp Waxweiler on 09.01.18.
+//  Copyright © 2018 Philipp Waxweiler. All rights reserved.
+//
+
+import Foundation
