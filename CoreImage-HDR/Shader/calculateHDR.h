e33bab3b557b2f0eedb752e9e9ce17559a82c6b5
Philipp Waxweiler <philipp@waxweiler.de>
2018-01-15 20:13:26 +0100

    completely removed check for saturation as the weight functions intention is to remove saturated pixels

--- CoreImage-HDR/Shader/calculateHDR.h
+++ CoreImage-HDR/Shader/calculateHDR.h
@@ -13,17 +13,11 @@ using namespace metal;
 inline half3 HDRValue(array_ref<half3> linearPixelArray, array_ref<uchar3> indices, constant float * t, constant float3 * W) {
     half3 zaehler = 0.0, nenner = 0.0, result = 0.0, weight;
     
-    if(all(indices[0].rgb != 255)){ // if darkest pixel is saturated...
-        // read out array and calculate HDR value
-        for(uint i = 0; i < linearPixelArray.size(); i++){ // iterate through all images at position gid
-            weight.rgb = half3( W[indices[i].r].r, W[indices[i].g].g, W[indices[i].b].b );
-            zaehler += weight.rgb * t[i] * linearPixelArray[i].rgb;
-            nenner += weight.rgb * (t[i] * t[i]);
-        }
-        result = zaehler / nenner;
-    } else {
-        result = 0; // ... tag it as Infinity.
+    for(uint i = 0; i < linearPixelArray.size(); i++){ // iterate through all images at position gid
+        weight.rgb = half3( W[indices[i].r].r, W[indices[i].g].g, W[indices[i].b].b );
+        zaehler += weight.rgb * t[i] * linearPixelArray[i].rgb;
+        nenner += weight.rgb * (t[i] * t[i]);
     }
     
-    return result;
+    return zaehler / nenner;
 }
