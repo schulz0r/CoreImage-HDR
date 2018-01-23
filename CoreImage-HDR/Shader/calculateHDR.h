546fb1ea00d83c8a4ca49ea4966181a30314b0b0
Philipp Waxweiler <philipp@waxweiler.de>
2018-01-23 18:29:37 +0100

    response ist smoother if pixels are rejected which contain any saturated pixel

--- CoreImage-HDR/Shader/calculateHDR.h
+++ CoreImage-HDR/Shader/calculateHDR.h
@@ -13,11 +13,12 @@ using namespace metal;
 inline half3 HDRValue(array_ref<half3> linearPixelArray, array_ref<uchar3> indices, constant float * t, constant float3 * W) {
     half3 zaehler = 0.0, nenner = 0.0, weight;
     
-    for(uint i = 0; i < linearPixelArray.size(); i++){ // iterate through all images at position gid
-        weight.rgb = half3( W[indices[i].r].r, W[indices[i].g].g, W[indices[i].b].b );
-        zaehler += weight.rgb * t[i] * linearPixelArray[i].rgb;
-        nenner += weight.rgb * (t[i] * t[i]);
+    if(!any(indices[0] == 255)){
+        for(uint i = 0; i < linearPixelArray.size(); i++){ // iterate through all images at position gid
+            weight.rgb = half3( W[indices[i].r].r, W[indices[i].g].g, W[indices[i].b].b );
+            zaehler += weight.rgb * t[i] * linearPixelArray[i].rgb;
+            nenner += weight.rgb * (t[i] * t[i]);
+        }
     }
-    
     return zaehler / nenner;
 }
