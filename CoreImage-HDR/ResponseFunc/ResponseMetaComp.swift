f3d3610b3db5a1e9514f7275966d3856f1457aa1
Philipp Waxweiler <philipp@waxweiler.de>
2018-03-10 00:57:09 +0100

    removed metal memory allocations in response shader, using shadrIOs instead

--- CoreImage-HDR/ResponseFunc/ResponseMetaComp.swift
+++ CoreImage-HDR/ResponseFunc/ResponseMetaComp.swift
@@ -18,10 +18,8 @@ protocol MetaComputer {
 public final class ResponseEstimator: MetaComputer {
     var computer : HDRComputer
     
-    private var textures: [MTLTexture]! = nil
-    
     init() {
-        var assets = MTKPAssets(ResponseEstimator.self)
+        let assets = MTKPAssets(ResponseEstimator.self)
         self.computer = HDRComputer(assets: assets)
     }
     
@@ -30,48 +28,33 @@ public final class ResponseEstimator: MetaComputer {
             fatalError("Image bracket length must be at least 2 and 5 at maximum.")
         }
         
-        guard
-            let summationShader = computer.assets["writeMeasureToBins"],
-            let buffer = summationShader.buffers?[0],
-            let threadsForBinReductionShader = computer.assets["reduceBins"]?.tgConfig.tgSize,
-            let MTLCardinalityBuffer = computer.assets["reduceBins"]?.buffers?[3]
-        else {
-            fatalError()
-        }
-        
-        
         // create shared ressources
         let TGSizeOfSummationShader = (16, 16, 1)
         
         let Inputs = LDRImagesShaderIO(ImageBracket: ImageBracket, cameraShifts: cameraShifts)
-        let CameraParametersIO = CameraParametersShaderIO(cameraParameters: cameraParameters)
+        let CameraParametersIO = CameraParametersShaderIO(cameraParameters: &cameraParameters)
         let ResponseSummationAssets = ResponseSummationShaderIO(inputTextures: Inputs, camParameters: CameraParametersIO)
         
         // configure threadgroups for each shader
         let ResponseSummationThreadgroup = MTKPThreadgroupConfig(tgSize: TGSizeOfSummationShader, tgMemLength: [4 * TGSizeOfSummationShader.0 * TGSizeOfSummationShader.1])
         let bufferReductionThreadgroup = MTKPThreadgroupConfig(tgSize: (256,1,1))
         
+        let smoothResponseAssets = smoothResponseShaderIO(cameraParameterIO: CameraParametersIO, controlPointCount: cameraParameters.BSplineKnotCount)
+        let smoothResponseThreadgroup = MTKPThreadgroupConfig(tgSize: (256 / cameraParameters.BSplineKnotCount, 1, 1))
+        
         self.computer.assets.add(shader: MTKPShader(name: "writeMeasureToBins", io: ResponseSummationAssets, tgConfig: ResponseSummationThreadgroup))
         self.computer.assets.add(shader: MTKPShader(name: "reduceBins", io: ResponseSummationAssets, tgConfig: bufferReductionThreadgroup))
-        
-        
-        let smoothResponseAssets = smoothResponseShaderIO(cameraResponse: MTLResponseFunc, weightFunction: MTLWeightFunc, controlPointCount: cameraParameters.BSplineKnotCount)
-        let smoothResponseThreadgroup = MTKPThreadgroupConfig(tgSize: (256 / cameraParameters.BSplineKnotCount, 1, 1))
-        computer.assets.add(shader: MTKPShader(name: "smoothResponse", io: smoothResponseAssets, tgConfig: smoothResponseThreadgroup))
-        
-        memcpy(self.MTLResponseFunc.contents(), cameraParameters.responseFunction, cameraParameters.responseFunction.count * MemoryLayout<float3>.size)
-        memcpy(self.MTLWeightFunc.contents(), cameraParameters.weightFunction, cameraParameters.weightFunction.count * MemoryLayout<float3>.size)
+        self.computer.assets.add(shader: MTKPShader(name: "smoothResponse", io: smoothResponseAssets, tgConfig: smoothResponseThreadgroup))
         
         computer.commandBuffer = MTKPDevice.commandQueue.makeCommandBuffer()
-        
-        textures.forEach({ texture in
-            computer.encodeMPSHistogram(forImage: texture, MTLHistogramBuffer: MTLCardinalityBuffer)
+        Inputs.fetchTextures()!.forEach({ texture in
+            computer.encodeMPSHistogram(forImage: texture!, MTLHistogramBuffer: ResponseSummationAssets.fetchBuffers()![2])
         })
         
         (0..<iterations).forEach({ iterationIdx in
             computer.encode("writeMeasureToBins")
-            computer.encode("reduceBins", threads: threadsForBinReductionShader)
-            computer.flush(buffer: buffer)
+            computer.encode("reduceBins", threads: bufferReductionThreadgroup.tgSize)
+            computer.flush(buffer: ResponseSummationAssets.fetchBuffers()![0])
         })
         
         // if command buffer is not committed here, the smooth shader will not be
@@ -84,9 +67,9 @@ public final class ResponseEstimator: MetaComputer {
         computer.commandBuffer.commit()
         computer.commandBuffer.waitUntilCompleted()
         
-        cameraParameters.responseFunction = Array(UnsafeMutableBufferPointer(start: self.MTLResponseFunc.contents().assumingMemoryBound(to: float3.self), count: 256))
+        cameraParameters.responseFunction = Array(UnsafeMutableBufferPointer(start: CameraParametersIO.fetchBuffers()![0].contents().assumingMemoryBound(to: float3.self), count: 256))
         cameraParameters.responseFunction = cameraParameters.responseFunction.map{$0 / cameraParameters.responseFunction[127]}
-        cameraParameters.weightFunction = Array(UnsafeMutableBufferPointer(start: self.MTLWeightFunc.contents().assumingMemoryBound(to: float3.self), count: 256))
+        cameraParameters.weightFunction = Array(UnsafeMutableBufferPointer(start: CameraParametersIO.fetchBuffers()![1].contents().assumingMemoryBound(to: float3.self), count: 256))
         let Max = float3(cameraParameters.weightFunction.map{$0.x}.max()!,
                          cameraParameters.weightFunction.map{$0.y}.max()!,
                          cameraParameters.weightFunction.map{$0.z}.max()!)
